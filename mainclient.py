816830aeb738fabf127046c29b44fb4cf9284fbb
STALKER <sina.golshahi@gmail.com>
2023-06-25 07:54:13 +0330

    mains ready for use in front-end

--- mainclient.py
+++ mainclient.py
@@ -1,9 +1,55 @@
 import socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 9999))
+def connecting_to_server() :
+    """make sures that the client is connected to the server
+    [client] = A subclass of _socket.socket(for more info type help(client))"""
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try :
+        client.connect(("localhost", 9999))
+    except Exception :
+        print("Error [connection to the server failed]")
+        return (False,"[connection to the server failed]")
+    #message = client.recv(1024).decode()
+    #client.send(input(message).encode())
+    #message = client.recv(1024).decode()
+    #client.send(input(message).encode())
+    #print(client.recv(1024).decode())
 
-message = client.recv(1024).decode()
-client.send(input(message).encode())
-message = client.recv(1024).decode()
-client.send(input(message).encode())
-print(client.recv(1024).decode())
+
+
+    def signin(username,password) :
+        ORDER = "signin"
+        """signin from the client side
+        [username] = the username that will be sent to server to check validity
+        [password] = the password that will be sent to server to check validity
+        [ORDER] = the order we are giving to the server to what to do
+        [signin] = a def for signing in
+        [signup] = a def for signing up
+        [disconnection] = a def for disconnecting"""
+        client.send(f"('{ORDER}' ,'{str(username)}' ,'{str(password)}')".encode())
+        print(client.recv(2048).decode())
+
+    def signup(username,password,password_again) :
+        ORDER = "signup"
+        """signup from the client side
+        [ORDER] = the order we are giving to the server to what to do
+        [username] = the username that will be sent to server to check validity
+        [password] = the password that will be sent to server to check validity"""
+        client.send(f"('{ORDER}' ,'{str(username)}' ,'{str(password)}' ,'{str(password_again)}')".encode())
+        receiver = client.recv(2048).decode()
+        print(receiver)
+        return receiver
+
+    def disconnection() :
+        """disconnection for the client to end its connection"""
+        client.send('!DISCONNECT'.encode())
+        return 0
+
+    return (True, signin, signup, disconnection)
+
+
+main_connection_to_server = connecting_to_server()
+
+if main_connection_to_server[0] == True :
+    main_connection_to_server[1]("hashembandari","lolliipopopop")
+    main_connection_to_server[3]()
+    
--- mainserver.py
+++ mainserver.py
@@ -2,27 +2,77 @@ import sqlite3
 import hashlib
 import socket
 import threading
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("localhost",9999))
-
 server.listen()
+print("[LISTENING] server is listening...")
 
-def handle_connection(c) :
-    c.send("Username: ".encode())
-    username = c.recv(1024).decode()
-    c.send("Password: ".encode())
-    password = c.recv(1024)
-    password = hashlib.sha256(password).hexdigest()
+def client_handler(c) :
+    """client will be handled here meaning that everything that has to be sent to it and anything that has to 
+    be recieved here will be handled
+    [c] = the connection"""
+    #c.send("Username: ".encode())
+    #username = c.recv(1024).decode()
+    #c.send("Password: ".encode())
+    #password = c.recv(1024)
+    
+    connected = True
+    while connected:
+        income = c.recv(1024).decode()
+        if income:
+            if income == "!DISCONNECT" :
+                connected = False
+            else :
+                content = eval(income)
+                if content[0] == "signin" :
+                    username ,password = content[1:]
+                    password = hashlib.sha256(password.encode()).hexdigest()
+                    signin(c,username,password)
+                
+                elif content[0] == "signup" :
+                    username ,password, password_again = content[1:]
+                    password = hashlib.sha256(password.encode()).hexdigest()
+                    password_again = hashlib.sha256(password_again.encode()).hexdigest()
+                    signup(c,username,password,password_again)#password again
+
+
+def signin(c,username ,password) :
+    """signin from the server side 
+    [conn] = connection with the database
+    [cur] = a control structure for traversal over the database records
+    [c] = connection we have with our client
+    [username] = username that was sent from client
+    [password] = password that was sent from the client"""
 
     conn = sqlite3.connect("userdata.df")
     cur = conn.cursor()
     cur.execute("SELECT * FROM userdata WHERE username = ? and password = ?", (username,password))
     if cur.fetchall() :
-        c.send("Login successful".encode())
-    
+        c.send(f"Login for {c.getpeername()[0]} was successful".encode())
     else :
         c.send("Login failed!".encode())
 
+
+def signup(c,username,password,password_again) :
+    """signup from the server side 
+    [conn] = connection with the database
+    [cur] = a control structure for traversal over the database records
+    [c] = connection we have with our client
+    [username] = username that was sent from client
+    [password] = password that was sent from the client"""
+
+    conn = sqlite3.connect("userdata.df")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM userdata WHERE username = ?", (username,))
+    if cur.fetchall() :
+        c.send("[CONFLICT] This username has been taken...".encode())
+    else :
+        cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)",(username,password))
+        c.send("Signup successfull!".encode())
+        conn.commit()
+
 while True :
     client, addr = server.accept()
-    threading.Thread(target= handle_connection, args = (client,)).start()
+    threading.Thread(target= client_handler, args = (client,)).start()
+    print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
